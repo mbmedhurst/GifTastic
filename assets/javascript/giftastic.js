aad2d4134eb36966d1b6f0fe0fb16c05e6626adf
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-03-22 09:44:18 -0700

    restyling

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,58 +1,49 @@
 // giphy api key: 2C01k7c92UOBlnlnBpB2y3P6B4Vdmxw7
 
-// code to handle blank submission into search field
-// const animal = document.querySelector("inputDiv").value.length >= 1 {
-//     run fetch
-// } else {
-//     display text, e.g. please enter the name of an animal
-// }
-
-// user enters an animal name into the search field
-// on clicking submit
-// -- adds a new button with the id of the animal name they entered, to the #displayBtn div 
-// -- clicking on that button fires the ajax request using ${animal}
-// clicking on any gif will pause/play the gif
-
-
-
+// array of animal names entered into the input field
 let animalArr = []
 
+// text entered into input field is pushed to animal array
+document.querySelector('#submit').addEventListener('click', e => {
+    // prevents page reload upon submit/enter
+    e.preventDefault()
+    // logic to prevent empty submission
+    if (document.querySelector('#animal').value.length >= 1){
+        animalArr.push(document.querySelector('#animal').value)
+        renderButtons()
+    }
+    // input field is cleared upon submit/enter
+    document.querySelector('#animal').value = ''
+})
+
 // most of this taken from the To Do List activity
+// buttons are rendered from user's input
 const renderButtons = _ => {
     document.querySelector('#displayBtn').innerHTML = ''
     animalArr.forEach((item, i) => {
         let animalArrElem = document.createElement('button')
         animalArrElem.textContent = item
-        animalArrElem.className = 'animal'
-        animalArrElem.setAttribute('data-animalaArrLoc', i)
+        animalArrElem.className = 'animalBtn'
+        animalArrElem.setAttribute('data-animalArrLoc', i)
+        document.querySelector('#displayBtn').addEventListener('click', function (e) {
+            console.log(e.target.dataset.animalarrloc)
+        })
         document.querySelector('#displayBtn').append(animalArrElem)
     })
 }
 
-// text entered into input field is pushed to animal array
-// input field is cleared upon submit/enter
-document.querySelector('#submit').addEventListener('click', e => {
-    e.preventDefault()
-    animalArr.push(document.querySelector('#animal').value)
-    renderButtons()
-    document.querySelector('#animal').value = ''
-})
-
-
 // fetch request for animal gifs
-
-// const getGif = animal => {
-//     event.preventDefault()
-//     fetch (`https://api.giphy.com/v1/gifs/search?api_key=2C01k7c92UOBlnlnBpB2y3P6B4Vdmxw7&q=${animal}&limit=10&rating=g`)
-//     .then(r => r.json())
-//     .then(({data}) => {
-//         document.querySelector('#displayGif').innerHTML = ''
-//         data.forEach(gif => {
-//         const url = gif.images.fixed_height.url
-//         let gifElem = document.createElement('img')
-//         gifElem.setAttribute('src', url)
-//         document.querySelector('#displayGif').append(gifElem)
-//         })
-//     })
-//     .catch(e => console.log(e))
-// } 
+const getGif = () => {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=2C01k7c92UOBlnlnBpB2y3P6B4Vdmxw7&q=${animal}&limit=10&rating=g`)
+        .then(r => r.json())
+        .then(({ data }) => {
+            document.querySelector('#displayGif').innerHTML = ''
+            data.forEach(gif => {
+                const url = gif.images.fixed_height.url
+                let gifElem = document.createElement('img')
+                gifElem.setAttribute('src', url)
+                document.querySelector('#displayGif').append(gifElem)
+            })
+        })
+        .catch(e => console.log(e))
+}
